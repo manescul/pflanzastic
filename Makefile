47331422fcf69066f1adefe3e79c9b75dec3bd26
Lucian Manescu <lmanescu@gmail.com>
2021-03-19 21:08:25 +0100

    initial code

---
+++ Makefile
@@ -0,0 +1,10 @@
+up: fe-build
+	docker-compose up -d
+
+fe-build:
+	elm make src/fe/Pflanzastic.elm --output=public/assets/elm.js
+
+down:
+	docker-compose down
+
+reset: down up
---
+++ src/fe/Pflanzastic.elm
@@ -0,0 +1,61 @@
+module Pflanzastic exposing (..)
+
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation exposing (Key)
+import Html exposing (Html)
+import Url exposing (Url)
+
+type alias Flags = 
+    {}
+
+type alias Model =
+    {name : String}
+
+type Msg
+    = NoOp
+
+init : Flags -> Url -> Key -> (Model, Cmd Msg)
+init flags url key =
+    let
+        _ = Debug.log "url" url 
+    in
+        (Model "Start water plants", Cmd.none)
+
+view : Model -> Document Msg
+view model =
+    {title = "Pflanzastic"
+    ,body = 
+        [
+                Html.p [] [Html.text "Pflanzastic"]
+        ]
+    }
+
+
+onUrlRequest : UrlRequest -> Msg
+onUrlRequest urlRequest =
+    NoOp
+
+
+onUrlChange : Url -> Msg
+onUrlChange url =
+    NoOp
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    (model, Cmd.none)
+
+main = 
+    Browser.application 
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        , onUrlChange = onUrlChange
+        , onUrlRequest = onUrlRequest
+        }
