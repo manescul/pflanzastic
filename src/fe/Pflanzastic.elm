970a7b16ad127c20593381507a0345bd8a831421
Lucian Manescu <lmanescu@gmail.com>
2021-03-20 12:17:42 +0100

    Relocate docker-compose file

--- src/fe/Pflanzastic.elm
+++ src/fe/Pflanzastic.elm
@@ -5,28 +5,33 @@ import Browser.Navigation exposing (Key)
 import Html exposing (Html)
 import Url exposing (Url)
 
-type alias Flags = 
+
+type alias Flags =
     {}
 
+
 type alias Model =
-    {name : String}
+    { name : String }
+
 
 type Msg
     = NoOp
 
-init : Flags -> Url -> Key -> (Model, Cmd Msg)
+
+init : Flags -> Url -> Key -> ( Model, Cmd Msg )
 init flags url key =
     let
-        _ = Debug.log "url" url 
+        _ =
+            Debug.log "url" url
     in
-        (Model "Start water plants", Cmd.none)
+    ( Model "Start water plants", Cmd.none )
+
 
 view : Model -> Document Msg
 view model =
-    {title = "Pflanzastic"
-    ,body = 
-        [
-                Html.p [] [Html.text "Pflanzastic"]
+    { title = "Pflanzastic"
+    , body =
+        [ Html.p [] [ Html.text "Pflanzastic" ]
         ]
     }
 
@@ -46,12 +51,13 @@ subscriptions model =
     Sub.none
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    (model, Cmd.none)
+    ( model, Cmd.none )
 
-main = 
-    Browser.application 
+
+main =
+    Browser.application
         { init = init
         , update = update
         , view = view
